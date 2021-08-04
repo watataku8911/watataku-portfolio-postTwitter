b400bd480e673b8bab14b79478161348d754f602
Watataku <39507718+watataku8911@users.noreply.github.com>
2021-08-04 17:47:20 +0900

    code fix

--- index.js
+++ index.js
@@ -1,89 +1,84 @@
 const express = require("express");
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 app.use(cors());
 
-const Twitter = require("twitter")
+const Twitter = require("twitter");
 
-const getCustomerKey = require("./seacretDirectory/customerKey")
-const getCustomerSeacret = require("./seacretDirectory/customerSeacret")
-const getAccsessToken = require("./seacretDirectory/accsessTokenKey")
-const getAccsessTokenSeacret = require("./seacretDirectory/accsessTorknSeacret")
+const getCustomerKey = require("./seacretDirectory/customerKey");
+const getCustomerSeacret = require("./seacretDirectory/customerSeacret");
+const getAccsessToken = require("./seacretDirectory/accsessTokenKey");
+const getAccsessTokenSeacret = require("./seacretDirectory/accsessTorknSeacret");
 
-app.get('/api', (req, res) => {
-	console.log();
-	res.json({"message": "Bad Request"});
+const client = new Twitter({
+  consumer_key: getCustomerKey(),
+  consumer_secret: getCustomerSeacret(),
+  access_token_key: getAccsessToken(),
+  access_token_secret: getAccsessTokenSeacret(),
 });
 
-app.post('/api', (req, res) => {
-	const detail_id = req.query.id;
-
-	if(detail_id === "") {
-		res.json({"message": "Bad Request"});
-	} else {
-		let client = new Twitter({
-			consumer_key: getCustomerKey(),
-			consumer_secret: getCustomerSeacret(),
-			access_token_key: getAccsessToken(),
-			access_token_secret: getAccsessTokenSeacret()
-		});
-	
-		const params = {
-			status:
-				"以下のものを作成しました。\n" +
-				"詳しくは下記サイトへ。\n" +
-				"https://watataku-portfolio.web.app/works/detail/" +
-				detail_id +
-				"/page/1/categoryId/0",
-		};
-	  
-		client.post('statuses/update', params, ((error, tweet) =>　{
-			if (!error) {
-				res.json(tweet);
-			} else {
-				res.json(error);
-			}
-		}));
-	}
+//get
+app.get("/api", (req, res) => {
+  res.json({ message: "Bad Request" });
 });
 
-app.put('/api', (req, res) => {
-	const detail_id = req.query.id;
-	if(detail_id === "") {
-		res.json({"message": "Bad Request"});
-	} else {
-		let client = new Twitter({
-			consumer_key: getCustomerKey(),
-			consumer_secret: getCustomerSeacret(),
-			access_token_key: getAccsessToken(),
-			access_token_secret: getAccsessTokenSeacret()
-		});
-	
-		const params = {
-			status:
-				"以下のものを作成しました。\n" +
-				"詳しくは下記サイトへ。\n" +
-				"https://watataku-portfolio.web.app/works/detail/" +
-				detail_id +
-				"/page/1/categoryId/0",
-		};
-	  
-		client.post('statuses/update', params, ((error, tweet) =>　{
-			if (!error) {
-				res.json(tweet);
-			} else {
-				res.json(error);
-			}
-		}));
-	}
+//post
+app.post("/api", (req, res) => {
+  const detail_id = req.query.id;
+
+  if (detail_id === "") {
+    res.json({ message: "Bad Request" });
+  } else {
+    const params = {
+      status:
+        "以下のものを作成しました。\n" +
+        "詳しくは下記サイトへ。\n" +
+        "https://watataku-portfolio.web.app/works/detail/" +
+        detail_id +
+        "/page/1/categoryId/0",
+    };
+
+    client.post("statuses/update", params, (error, tweet) => {
+      if (!error) {
+        res.json(tweet);
+      } else {
+        res.json(error);
+      }
+    });
+  }
 });
 
-app.delete('/api', (req, res) => {
-	res.json({"message": "Bad Request"});
+//put
+app.put("/api", (req, res) => {
+  const detail_id = req.query.id;
+  if (detail_id === "") {
+    res.json({ message: "Bad Request" });
+  } else {
+    const params = {
+      status:
+        "以下のものを作成しました。\n" +
+        "詳しくは下記サイトへ。\n" +
+        "https://watataku-portfolio.web.app/works/detail/" +
+        detail_id +
+        "/page/1/categoryId/0",
+    };
+
+    client.post("statuses/update", params, (error, tweet) => {
+      if (!error) {
+        res.json(tweet);
+      } else {
+        res.json(error);
+      }
+    });
+  }
+});
+
+//delete
+app.delete("/api", (req, res) => {
+  res.json({ message: "Bad Request" });
 });
 
 const port = process.env.PORT || 3080;
 app.listen(port);
 console.log("ポート番号" + port + "でWebサーバが立ち上がりました");
-
